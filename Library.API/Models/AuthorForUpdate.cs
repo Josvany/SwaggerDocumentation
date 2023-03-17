d76fdc6719d21bd915014b6f4db0c0705457a134
Josvany_G <josvanyg@gmail.com>
2023-03-17 13:56:01 -0600

    mejora con anotacion de datos

--- Library.API/Models/AuthorForUpdate.cs
+++ Library.API/Models/AuthorForUpdate.cs
@@ -1,9 +1,23 @@
-﻿namespace Library.API.Models
-{
-    public class AuthorForUpdate
-    {  
-        public string? FirstName { get; set; }
+﻿using System.ComponentModel.DataAnnotations;
 
+namespace Library.API.Models
+{
+    /// <summary>
+    /// actor update
+    /// </summary>
+    public class AuthorForUpdate
+    {
+        /// <summary>
+        /// nombre actor
+        /// </summary>
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [MaxLength(100)]
+        public string? FirstName { get; set; }
+        /// <summary>
+        /// apellido actor
+        /// </summary>
+        [Required(ErrorMessage = "El apellido es requerido")]
+        [MaxLength(100)]
         public string? LastName { get; set; }
     }
 }
