468278e6c33b7f8088dc62e581f3fc748ec60d6a
Josvany_G <josvanyg@gmail.com>
2023-03-17 13:51:54 -0600

    comentarios en clase modelo

--- Library.API/Models/Author.cs
+++ Library.API/Models/Author.cs
@@ -2,13 +2,22 @@
 
 namespace Library.API.Models
 {
-
+    /// <summary>
+    /// author
+    /// </summary>
     public class Author
-    {        
-        public Guid Id { get; set; } 
-     
+    {
+        /// <summary>
+        /// Id del author
+        /// </summary>
+        public Guid Id { get; set; }
+        /// <summary>
+        /// Nombre
+        /// </summary>
         public string? FirstName { get; set; }
-      
+        /// <summary>
+        /// apellido
+        /// </summary>
         public string? LastName { get; set; }
     }
 }
